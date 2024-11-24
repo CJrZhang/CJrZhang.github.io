b22ded698435795884e94bd4c7497b7b5849a1f0
ironickzhang <ironickzhang@outlook.com>
2024-11-24 20:52:20 +0800

    [Feature] Change title style

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ function App() {
 
   return (
     <>
-      <h1 className='font-mono text-center'>IroNick Zhang's Homepage</h1>
+      <h1 className='font-mono text-center leading-normal font-normal text-[4em] 
+        text-white bg-[#00b294]'>
+        IroNick Zhang's Homepage
+      </h1>
     </>
   )
 }
