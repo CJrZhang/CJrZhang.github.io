ef91f72090ad82b16541bf9ce3928ca888d53805
ironickzhang <ironickzhang@outlook.com>
2024-11-24 13:24:33 +0800

    [Feature] Add tailwind dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <h1>IroNick Zhang's Homepage</h1>
+      <h1 className='font-mono text-center'>IroNick Zhang's Homepage</h1>
     </>
   )
 }
